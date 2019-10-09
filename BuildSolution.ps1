a5d6d633af14e0ffbd51b9399b83f7f41c0684ff
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-10-09 15:51:01 +0100

    #11 Build off tags if doing beta installers

--- BuildSolution.ps1
+++ BuildSolution.ps1
@@ -21,5 +21,48 @@ If ($env:PatchVersion) {
   $patchVersion = $env:PatchVersion
 }
 
+If($releaseType -eq "beta")
+{
+	# Try and checkout tags
+	
+	$version = $ENV:Version
+
+	$cwd = Get-Location
+
+	Write-Output("Changing into repo directory")
+	Set-Location $ENV:BUILD_SOURCESDIRECTORY\BHoM_Installer
+
+	# Update repo to get latest tags
+	git fetch
+	$tags = git tag -l
+	$repoTags = $tags.split(" ")
+	$tag = ""
+
+	For ($i = $repoTags.Length - 1; $i -ge 0; $i--)
+	{
+    	$splitTag = $repoTags[$i].split(".")
+    	If(($splitTag[0] + "." + $splitTag[1]) -eq $version)
+    	{
+    		If($splitTag[3] -le $patchVersion)
+    		{
+    			$tag = $repoTags[$i]
+    			break
+    		}
+    	}
+    }
+
+    If($tag -eq "")
+    {
+    	Write-Output("A suitable Tag for " + $version + ".B." + $patchVersion + " could not be found, staying on master")
+    }
+    Else
+    {
+		Write-Output ("Checking out at tag " + $tag)
+		git checkout -q tags/$tag
+	}
+	
+	Write-Output ("Changing back directory location")
+	Set-Location $cwd
+}
 
 & $msbuildPath -nologo /verbosity:minimal /p:RunWixToolsOutOfProc=true /p:DeployOnBuild=true /p:ReleaseType=$releaseType /p:PatchVersion=$patchVersion /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:SkipInvalidConfigurations=true
